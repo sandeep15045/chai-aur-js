cc95bd6ba7ef2a311317e71c4db1ee8de77b61a1
Sandeep Kumar <sandeep15045@gmail.com>
2023-11-08 11:05:54 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+//earlier there was no arrow function, classes, modules in js etc
+"use strict"; // treat all js code as newer version -well now its automatically treated as new
+// alert('hello') not working here as we are using nodejs not  browser
+// in most react node we avoid semicolon ;
+console.log(3+3); console.log('san'); 
+// tc39.es ecma original- and mdn by mozilla
+
+// symbol=> unique, number=>2 to power 53, bigint, string=> " ", boolean=> true/false, null=>standalone value, undefined=>
+// above was primitive
+
+//object
+console.log(typeof null); //object is type of null
+console.log(typeof undefined);
