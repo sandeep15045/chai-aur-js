61b4b1ec42e9bfe3f557c8a846976d7c6075a9a9
Sandeep Kumar <sandeep15045@gmail.com>
2023-11-08 10:46:25 +0530

    1 let_var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID=1221;
+let accountEmail="sandeep"
+var accountPassword="234";
+accountCity="Agra"
+let accountState;
+
+// prefer not to use var becoz of issue in block scope and functional scope
+
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
